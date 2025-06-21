05627f1715f3d111da7c6ad0d83c2b8568b27848
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-06-21 12:42:43 +0600

    add

---
+++ Binary_Search/BInary_search_lower_upper_bound.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerBound(vector<int>&a,int n,int x){
+int low=0;
+int high=n;
+while(low<high){
+    int mid=(low+high)/2;
+    if(a[mid]>=x) high=mid;
+    else low=mid+1;
+}
+if(low<n && a[low]<x)low++;
+return low;
+}
+int uperBound(vector<int>&a,int n,int x){
+int low=0;
+int high=n;
+while(low<high){
+    int mid=(low+high)/2;
+    if(a[mid]>x) high=mid;
+    else low=mid+1;
+}
+if(low<n && a[low]<x)low++;
+return low;
+}
+int main(){
+vector<int>a;
+int n;
+cin>>n;
+cout<<endl;
+for(int i=0;i<n;i++){
+    int x;
+    cin>>x;
+    a.push_back(x);
+}
+int element=3;
+int l=uperBound(a,n,element);
+if(l==n) cout<<"not found"<<endl;
+else cout<<l<<endl;
+}
